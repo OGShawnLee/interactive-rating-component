405cfe9a80c94c697ffa45f1bda55496ea3cd357
Shawn Lee <changoloco.evg@gmail.com>
2022-04-01 22:35:45 -0600

    chore: add assets path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
-import windicss from 'vite-plugin-windicss'
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import windicss from 'vite-plugin-windicss';
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte(), windicss()]
-})
+  plugins: [svelte(), windicss()],
+  resolve: {
+    alias: {
+      $assets: resolve('src/assets'),
+      '$assets/*': resolve('src/assets/*'),
+    },
+  },
+});

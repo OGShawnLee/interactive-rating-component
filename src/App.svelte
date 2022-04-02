2425544838f3226ce7fae7c0dcef76cc9714f4fe
Shawn Lee <changoloco.evg@gmail.com>
2022-04-01 19:58:25 -0600

    feat: initial commit

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+    <!-- Rating state start -->
+
+    How did we do? Please let us know how we did with your support request. All feedback
+    is appreciated to help us improve our offering! 1 2 3 4 5 Submit
+
+    <!-- Rating state end -->
+
+    <!-- Thank you state start -->
+
+    You selected
+    <!-- Add rating here -->
+    out of 5 Thank you! We appreciate you taking the time to give a rating. If you ever
+    need more support, don’t hesitate to get in touch!
+
+    <!-- Thank you state end -->

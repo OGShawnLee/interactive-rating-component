d30741dd535a9565ef340d418c7d23104fd90b39
OGShawnLee <changoloco.evg@gmail.com>
2022-06-03 14:05:46 +0000

    :adhesive_bandage: refactor: export thank you illustration properly

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,2 +1,2 @@
 export { default as iStar } from './icon-star.svg';
-export { default as illustrationThankYou } from './icon-star.svg';
+export { default as illustrationThankYou } from './illustration-thank-you.svg';

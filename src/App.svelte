58f906e95c5663b975b94722ee073c2e46051908
OGShawnLee <changoloco.evg@gmail.com>
2022-06-03 12:48:11 +0000

    :art: style: prettier

--- src/App.svelte
+++ src/App.svelte
@@ -38,17 +38,19 @@
   {#if state === 'idle'}
     <div
       class="absolute over-center w-full max-w-xs sm:max-w-sm p-8 | bg-slate-800/80 rounded-2xl"
-      transition:fade>
+      transition:fade
+    >
       <form class="flex flex-col gap-6" on:submit|preventDefault={handleSubmit}>
         <div
-          class="h-10 w-10 min-w-10 sm:(h-12 w-12 min-w-12) | grid place-content-center | bg-slate-500/15 rounded-full">
+          class="h-10 w-10 min-w-10 sm:(h-12 w-12 min-w-12) | grid place-content-center | bg-slate-500/15 rounded-full"
+        >
           <img src={iconStar} alt="" />
         </div>
         <div class="flex flex-col gap-3">
           <span class="text-2xl font-bold text-white">How did we do?</span>
           <p class="text-[15px] leading-relaxed">
-            Please let us know how we did with your support request. All feedback is
-            appreciated to help us improve our offering!
+            Please let us know how we did with your support request. All feedback is appreciated to
+            help us improve our offering!
           </p>
         </div>
         <div class="flex justify-between">
@@ -58,14 +60,16 @@
                 index === rating
               )} | flex items-center justify-center | rounded-full text-sm"
               type="button"
-              on:click={() => (rating = index)}>
+              on:click={() => (rating = index)}
+            >
               {index}
             </button>
           {/each}
         </div>
         <button
           class="py-3 | bg-orange rounded-full sm:text-lg text-white uppercase tracking-wider hover:(bg-white text-orange)"
-          type="submit">
+          type="submit"
+        >
           Submit
         </button>
       </form>
@@ -73,7 +77,8 @@
   {:else}
     <div
       class="absolute over-center w-full max-w-xs sm:max-w-sm p-8 | bg-slate-800/80 rounded-2xl"
-      transition:fade={{ delay: 300 }}>
+      transition:fade={{ delay: 300 }}
+    >
       <div class="flex flex-col items-center gap-6">
         <img src={illustationThankYou} alt="" />
         <div class="px-4 py-2 | bg-slate-500/15 rounded-full text-orange">
@@ -82,8 +87,8 @@
         <div class="flex flex-col items-center gap-3">
           <span class="text-2xl font-bold text-white">Thank you!</span>
           <p class="text-[15px] text-center leading-relaxed">
-            We appreciate you taking the time to give a rating. If you ever need more
-            support, don’t hesitate to get in touch!
+            We appreciate you taking the time to give a rating. If you ever need more support, don’t
+            hesitate to get in touch!
           </p>
         </div>
       </div>

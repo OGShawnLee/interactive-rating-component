3c8dffa8ba710529d53852a6ef01f8aec629324d
OGShawnLee <changoloco.evg@gmail.com>
2022-06-03 14:47:11 +0000

    :art: style: order classNames

--- src/App.svelte
+++ src/App.svelte
@@ -16,17 +16,17 @@
 <Rating as="main" class="overflow-hidden" let:rate let:form let:state>
   {#if state === 'IDLE'}
     <section
-      class="absolute position-center w-full max-w-xs sm:max-w-sm p-8 | bg-slate-800/80 rounded-2xl"
+      class="absolute position-center max-w-xs w-full p-8 | bg-slate-800/80 rounded-2xl | sm:max-w-sm"
       transition:fade
     >
       <form class="flex flex-col gap-6" use:form>
         <div
-          class="h-10 w-10 min-w-10 sm:(h-12 w-12 min-w-12) | grid place-content-center | bg-slate-500/15 rounded-full"
+          class="h-10 min-w-10 w-10 | grid place-content-center | bg-slate-500/15 rounded-full | sm:(h-12 min-w-12 w-12)"
         >
           <img src={iStar} alt="" />
         </div>
         <div class="flex flex-col gap-3">
-          <h2 class="text-2xl font-bold text-white">How did we do?</h2>
+          <h2 class="text-2xl text-white font-bold">How did we do?</h2>
           <p class="text-[15px] leading-relaxed">
             Please let us know how we did with your support request. All feedback is appreciated to
             help us improve our offering!
@@ -47,7 +47,7 @@
           {/each}
         </div>
         <button
-          class="py-3 | bg-orange rounded-full sm:text-lg text-white uppercase tracking-wider hover:(bg-white text-orange)"
+          class="py-3 | bg-orange rounded-full text-white uppercase tracking-wider | hover:(bg-white text-orange) | sm:text-lg"
           type="submit"
         >
           Submit
@@ -56,7 +56,7 @@
     </section>
   {:else}
     <section
-      class="absolute position-center w-full max-w-xs sm:max-w-sm p-8 | bg-slate-800/80 rounded-2xl"
+      class="absolute position-center w-full max-w-xs p-8 | bg-slate-800/80 rounded-2xl | sm:max-w-sm"
       transition:fade={{ delay: 300 }}
     >
       <div class="flex flex-col items-center gap-6">
@@ -66,7 +66,7 @@
         </div>
         <div class="flex flex-col items-center gap-3">
           <h2 class="sr-only">Support Rating Form Results</h2>
-          <span class="text-2xl font-bold text-white">Thank you!</span>
+          <span class="text-2xl text-white font-bold">Thank you!</span>
           <p class="text-[15px] text-center leading-relaxed">
             We appreciate you taking the time to give a rating. If you ever need more support, don't
             hesitate to get in touch!
